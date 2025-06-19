dbab85bf69d493a1b6f4902b725f5dc696c62594
송광호 <skh990427@naver.com>
2025-06-20 02:01:43 +0900

    Flexbox 알아보기

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import {Button, Platform, SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function HomeScreen() {
     return (
-        <SafeAreaView>
-            <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input}/>
                 <Text>텍스트</Text>
-                <Button title={"버튼이름"} onPress={()=> console.log('클릭됨!')}></Button>
-                <TextInput />
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input}/>
+                <Text>텍스트</Text>
             </View>
         </SafeAreaView>
     );
@@ -14,9 +17,20 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: 'yellow',
+    },
+    input: {
+        flex: 1,
+        borderWidth: 2,
+        borderColor: 'black',
+        height: 100,
+        width: 100
+    },
+    inputContainer: {
+        flex: 1,
         backgroundColor: 'red',
-        // margin: "10%",
-        marginHorizontal: 10,
-        marginVertical: 10,
-    }
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
 });
