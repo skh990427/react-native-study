33e6723ea9badc11bb7bb0fa30e0313424920714
송광호 <skh990427@naver.com>
2025-06-21 23:13:41 +0900

    스택 네비게이션-타이핑

--- src/navigation/AuthStackNavigator.tsx
+++ src/navigation/AuthStackNavigator.tsx
@@ -3,13 +3,20 @@ import {StyleSheet} from 'react-native';
 import {createStackNavigator} from "@react-navigation/stack";
 import AuthHomeScreen from "@/src/screens/AuthHomeScreen";
 import LoginScreen from "@/src/screens/LoginScreen";
+import {authNavigations} from "@/src/constant";
+
+
+export type AuthStackParamList = {
+    [authNavigations.AUTH_HOME]: undefined;
+    [authNavigations.LOGIN]: undefined;
+}
 
 function AuthStackNavigator() {
-    const Stack = createStackNavigator();
+    const Stack = createStackNavigator<AuthStackParamList>();
     return (
         <Stack.Navigator>
-            <Stack.Screen name="AuthHome" component={AuthHomeScreen}/>
-            <Stack.Screen name="Login" component={LoginScreen}/>
+            <Stack.Screen name={authNavigations.AUTH_HOME} component={AuthHomeScreen}/>
+            <Stack.Screen name={authNavigations.LOGIN} component={LoginScreen}/>
         </Stack.Navigator>
     )
 }
--- src/screens/AuthHomeScreen.tsx
+++ src/screens/AuthHomeScreen.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import {Button, SafeAreaView, StyleSheet, View} from "react-native";
+import {StackScreenProps} from "@react-navigation/stack";
+import {AuthStackParamList} from "@/src/navigation/AuthStackNavigator";
+import {authNavigations} from "@/src/constant";
 
-function AuthHomeScreen({navigation}: any) {
+type AuthHomeScreenProps = StackScreenProps<AuthStackParamList>;
+
+function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
     return (
         <SafeAreaView>
             <View>
-                <Button title="로그인 화면으로 이동" onPress={() => navigation.navigate("Login")}/>
+                <Button title="로그인 화면으로 이동" onPress={() => navigation.navigate(authNavigations.LOGIN)}/>
             </View>
         </SafeAreaView>
     );
